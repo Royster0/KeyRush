cd9f242042edd32346aaa13f45edfa7822383d67
Royster0 <royt.huynh@gmail.com>
2025-11-25 22:07:56 -0800

    caret "lag" slightly

--- src/components/typing_test/Caret.tsx
+++ src/components/typing_test/Caret.tsx
@@ -8,7 +8,7 @@ const Caret = memo(() => (
   <motion.div
     initial={{ opacity: 1 }}
     animate={{ opacity: 0.6 }}
-    transition={{ duration: 0.07 }}
+    transition={{ duration: 0.1 }}
     className="absolute w-0.5 bg-accent-foreground"
     layoutId="caret"
     style={{ height: "1.3em", left: "-2px" }}
